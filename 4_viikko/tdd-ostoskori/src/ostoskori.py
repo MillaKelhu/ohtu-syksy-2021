d0c5c67cfabd4e13d60161287e141716786eac0a
Milla Kelhu <milla.kelhu@gmail.com>
2021-11-29 21:22:16 +0200

    Exercise 5, subexercises 1-4

---
+++ 4_viikko/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,28 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        self.kori = []
+
+    def tavaroita_korissa(self):
+        return len(self.kori) 
+
+    def hinta(self):
+        return sum(ostos.hinta() for ostos in self.kori)
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        self.kori.append(Ostos(lisattava))
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
