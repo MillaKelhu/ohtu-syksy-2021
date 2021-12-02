6c735117e389955570488f1df946b674442abdfb
Milla Kelhu <milla.kelhu@gmail.com>
2021-12-03 01:06:21 +0200

    Refactor code for exercise 3

--- 5_viikko/tennis/src/tennis_game.py
+++ 5_viikko/tennis/src/tennis_game.py
@@ -1,57 +1,36 @@
+LOVE = 0
+POINT = 1
+THREE_POINTS = 3
+
 class TennisGame:
     def __init__(self, player1_name, player2_name):
-        self.player1_name = player1_name
-        self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.players = {player1_name: LOVE, player2_name: LOVE}
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
-        else:
-            self.m_score2 = self.m_score2 + 1
+        self.players[player_name] += POINT
 
     def get_score(self):
+        player1_score, player2_score = self.players.values()
         score = ""
-        temp_score = 0
+        points = ["Love", "Fifteen", "Thirty", "Forty"]
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
+        if player1_score == player2_score:
+            if player1_score > THREE_POINTS:
                 score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
             else:
-                score = "Win for player2"
-        else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+                score = f"{points[player1_score]}-All"
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+        elif max(player1_score, player2_score) > THREE_POINTS:
+            difference = abs(player1_score - player2_score)
+
+            leading_player = max(self.players, key = self.players.get)
+
+            if difference == POINT:
+                score = f"Advantage {leading_player}"
+            else:
+                score = f"Win for {leading_player}"
+
+        else:
+            score = f"{points[player1_score]}-{points[player2_score]}"
 
         return score
