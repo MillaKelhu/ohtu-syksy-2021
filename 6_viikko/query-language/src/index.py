2ef6baf4d55a53600a3e57b4f8134cae626c242f
Milla Kelhu <milla.kelhu@gmail.com>
2021-12-10 20:08:58 +0200

    Exercise 2, check class Not

--- 6_viikko/query-language/src/index.py
+++ 6_viikko/query-language/src/index.py
@@ -1,17 +1,25 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, All, Not, HasFewerThan
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
+#    Esimerkki matcher
+#    matcher = And(
+#        HasAtLeast(5, "goals"),
+#        HasAtLeast(5, "assists"),
+#        PlaysIn("PHI")
+#    )
+
+#    Tehtävä 2 matcher testaus
     matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(5, "assists"),
-        PlaysIn("PHI")
+        Not(HasAtLeast(1, "goals")),
+        PlaysIn("NYR")
     )
+
 
     for player in stats.matches(matcher):
         print(player)
